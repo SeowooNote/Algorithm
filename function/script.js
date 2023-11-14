7dac68c50e618042fd9d640c37d74499367046aa
SeowooNote <dkr0210@naver.com>
2023-11-15 00:33:48 +0900

    2일차, 3일차 - 배열과 오브젝트의 성능 평가, 문제 해결접근법, 문제 해결패턴

---
+++ function/script.js
@@ -0,0 +1,51 @@
+const pokemons = [
+    {
+        id: 1,
+        name: '이상해씨',
+        type: '풀',
+        url: 'images/000101.png'
+    },
+    {
+        id: 2,
+        name: '파이리',
+        type: '불',
+        url: 'images/000401.png'
+    },
+    {
+        id: 3,
+        name: '꼬부기',
+        type: '물',
+        url: 'images/000701.png'
+    },
+    {
+        id: 4,
+        name: '삐삐',
+        type: '페어리',
+        url: 'images/003501.png'
+    },
+    {
+        id: 5,
+        name: '팬텀',
+        type: '고스트 & 독',
+        url: 'images/003501.png'
+    },
+    {
+        id: 6,
+        name: '내루미',
+        type: '노말',
+        url: 'images/010801.png'
+    },
+    {
+        id: 7,
+        name: '잠만보',
+        type: '노말',
+        url: 'images/014301.png'
+    },
+    {
+        id: 8,
+        name: '망나뇽',
+        type: '드래곤 & 비행',
+        url: 'images/014901.png'
+    },
+]
+
